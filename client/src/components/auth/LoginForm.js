c277721cf15f1a78b3d370cdb3ae46b2e991e167
thuccv98 <thuccv98@gmail.com>
2021-09-12 23:25:35 +0700

    Complete frontend login logic: redirect, protectedRoute

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,16 +1,58 @@
+import { useContext, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import AlertMessage from '../layout/AlertMessage';
 
 const LoginForm = () => {
+  // Context
+  const { loginUser } = useContext(AuthContext);
+
+  // Router
+
+  // Loacal state
+  const [loginForm, setLoginForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  const { username, password } = loginForm;
+
+  const onChangeLoginForm = (e) =>
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    try {
+      const loginData = await loginUser(loginForm);
+      if (loginData.success) {
+      } else {
+        setAlert({ type: 'danger', message: loginData.message });
+        setTimeout(() => {
+          setAlert(null);
+        }, 4000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={login}>
+        <AlertMessage info={alert} />
+
         <Form.Group>
           <Form.Control
             type="text"
             placeholder="Username"
             name="username"
             required
+            value={username}
+            onChange={onChangeLoginForm}
           />
         </Form.Group>
         <Form.Group>
@@ -19,6 +61,8 @@ const LoginForm = () => {
             placeholder="Password"
             name="password"
             required
+            value={password}
+            onChange={onChangeLoginForm}
           />
         </Form.Group>
         <Button variant="success" type="submit">
--- client/src/views/Auth.js
+++ client/src/views/Auth.js
@@ -1,15 +1,31 @@
 import LoginForm from '../components/auth/LoginForm';
 import RegisterForm from '../components/auth/RegisterForm';
+import { AuthContext } from '../contexts/AuthContext';
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
 const Auth = ({ authRoute }) => {
+  const {
+    authState: { authLoading, isAuthenticated },
+  } = useContext(AuthContext);
+
   let body;
 
-  body = (
-    <>
-      {authRoute === 'login' && <LoginForm />}
-      {authRoute === 'register' && <RegisterForm />}
-    </>
-  );
+  if (authLoading)
+    body = (
+      <div className="d-flex justify-content-center mt-2">
+        <Spinner animation="border" variant="info" />
+      </div>
+    );
+  else if (isAuthenticated) return <Redirect to="/dashboard" />;
+  else
+    body = (
+      <>
+        {authRoute === 'login' && <LoginForm />}
+        {authRoute === 'register' && <RegisterForm />}
+      </>
+    );
 
   return (
     <div className="landing">
